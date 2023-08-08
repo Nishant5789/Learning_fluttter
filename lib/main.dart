68226c8d5b762a1f76391fdbc63855b31a89727c
Nishant5789 <nishantbhandari218@gmail.com>
2023-08-08 15:34:18 +0530

    Day 4 - Adding Images, Google Fonts | ElevatedButton | Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_dart.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_dart.dart';
 
 // entry point of execition
@@ -13,32 +14,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.light,
 
-        // if we choose dark theam then darktheme would be 
-        // execute and choose light then theme properties wokr
+      // if we choose dark theam then darktheme would be
+      // execute and choose light then theme properties wokr
 
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          // primarySwatch is not a Color . It's MaterialColor . Which means it's different shades of a color a material app will use. primaryColor is one of those shades. To be exact, primaryColor is normally equal to primarySwatch[500]
-          brightness: Brightness.light
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        // primarySwatch is not a Color . It's MaterialColor . Which means it's different shades of a color a material app will use. primaryColor is one of those shades. To be exact, primaryColor is normally equal to primarySwatch[500]
+        brightness: Brightness.light,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
 
-        darkTheme: ThemeData( 
-          brightness: Brightness.dark,
-          primarySwatch: Colors.pink
-        ),
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.pink),
 
-        // home: Homepage(),
-        // either you specifed home properties of material or any page with path "/" in routes
+      // home: Homepage(),
+      // either you specifed home properties of material or any page with path "/" in routes
 
-        initialRoute: "/",
+      initialRoute: "/login",
 
-        routes: {
-          '/': (context) => Homepage(),
-          '/login': (context) => Loginpage(),
-        },
+      routes: {
+        '/': (context) => Homepage(),
+        '/login': (context) => Loginpage(),
+      },
     );
   }
 }
--- lib/pages/login_dart.dart
+++ lib/pages/login_dart.dart
@@ -5,14 +5,59 @@ class Loginpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text("login page",
-        style: TextStyle(
-          color: Colors.indigo,
-          fontSize: 30,
-        )),
-      ),
-    );
+    return  Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/loginicon.jpg",
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 20.0,
+            ),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            )
+            ),
+           Padding(
+            //  padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+             child: Column(
+               children: [
+                 TextFormField(
+                  decoration: const InputDecoration(
+                    // border: OutlineInputBorder(),
+                    labelText: "Username",
+                    hintText: "Enter your username",
+                  )
+                     ),
+                 TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    // border: OutlineInputBorder(),
+                    labelText: "Password",
+                    hintText: "Enter your Password",
+                  )
+                     ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      print("click");
+                    },
+                    child: Text("Login"),
+                  ),
+                    
+               ],
+             ),
+           )
+          
+        ],
+      ));
   }
 }
