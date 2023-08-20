79d2bed1a0150d356d4d325c7bf97528333c87c0
Nishant5789 <nishantbhandari218@gmail.com>
2023-08-20 15:35:37 +0530

    learned listbuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_dart.dart';
 import 'package:flutter_application_1/utils/routes.dart';
+import 'package:flutter_application_1/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_dart.dart';
 
@@ -18,19 +19,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       themeMode: ThemeMode.light,
-
       // if we choose dark theam then darktheme would be
       // execute and choose light then theme properties wokr
 
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        // primarySwatch is not a Color . It's MaterialColor . Which means it's different shades of a color a material app will use. primaryColor is one of those shades. To be exact, primaryColor is normally equal to primarySwatch[500]
-        brightness: Brightness.light,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-
-      darkTheme:
-          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.pink),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
 
       // home: Homepage(),
       // either you specifed home properties of material or any page with path "/" in routes
--- lib/pages/home_dart.dart
+++ lib/pages/home_dart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_1/widgets/item_widget.dart';
+import '../models/catalog.dart';
 import '../widgets/drawer.dart';
 
 
@@ -10,16 +11,30 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     // check scanffold - https://www.fluttercampus.com/tutorial/9/flutter-scaffold/
+    final dummyList = List.generate(15, (index) => CatalogModel.items[0]);
     
     return Scaffold(
       appBar: AppBar( 
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to 31 day flutter"),
+      // body:  Center(
+      //   child: Container(
+      //     child: Text("Welcome to 31 day flutter"),
+      //   ),
+      // ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              // item: CatalogModel.items[index],
+              item: dummyList[index],
+            );
+          },
         ),
       ),
+
       drawer: MyDrawer(),
     );
   }
